29fe0dfa467662e94945db4aa3790e7dfe209b42
rmores <rafael.mores@gmail.com>
2009-02-08 02:47:02 -0200

    Added GPL info

--- brutegen.c
+++ brutegen.c
@@ -1,3 +1,21 @@
+/*
+ *  BruteGen - Bruteforce Wordlist Generator.
+    Copyright (C) 2009  Rafael F. M. Mores
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -72,6 +90,10 @@ int main(int argc, char **argv)
 
 	if (argc < 3)
 	{
+		printf("%s Copyright (C) 2009  Rafael F. M. Mores
+		This program comes with ABSOLUTELY NO WARRANTY;
+		This is free software, and you are welcome to redistribute it
+		under certain conditions; \n\n",argv[0]);
 		printf("USO: %s <numero de caracteres> <string de caracteres validos>\n",argv[0]);
 		return EXIT_FAILURE;
 	}
@@ -81,4 +103,5 @@ int main(int argc, char **argv)
 
 	return EXIT_SUCCESS;
 }
+
 
